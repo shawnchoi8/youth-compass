d4f5674abe48e7f0f74c78de90bc5d0ec53d32dd
shawnchoi8 <bychoise8@gmail.com>
2025-10-27 20:39:44 +0900

    Add Spring Boot backend project

---
+++ backend/src/main/java/com/youthcompass/backend/YouthCompassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youthcompass.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthCompassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthCompassBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/youthcompass/backend/YouthCompassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youthcompass.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthCompassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
